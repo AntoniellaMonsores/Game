80ccf871cd8ce0b12e52368d8fe95064bf4a56e7
Antoniella de Araújo Monsores <antoniella.monsores@gmail.com>
2022-03-05 13:00:34 -0300

    entidade do sistema

---
+++ game.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Game {
+
+    private string nome;
+    private string genero;
+    private int niveis;
+
+    public Game(string nome, string genero, int niveis)
+    {
+        this.nome = nome;
+        this.genero = genero;
+        this.niveis = niveis;
+    }
+
+    public void SetNome(string nome) {
+        this.nome = nome;
+    }
+
+    public void SetGenero(string genero) {
+        this.genero = genero;
+    }
+    
+    public void SetNiveis(int niveis) {
+        this.niveis = niveis;
+    }
+    
+    public string GetNome() { return nome; }
+
+    public string GetGenero() { return genero; }
+    
+    public int GetNiveis() { return niveis; }
+    
+    public override string ToString() {
+        return $"{nome} - {genero} - {niveis}";
+    }
+}
