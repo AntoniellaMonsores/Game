92b1f08908fd2cbc32e3d21c38e24a4207aca7ef
Antoniella de Araújo Monsores <antoniella.monsores@gmail.com>
2022-03-07 09:16:59 -0300

    novo teste

--- main.cs
+++ main.cs
@@ -4,19 +4,23 @@ class Program {
 
     public static void Main(string[] args)
     {
-        Game a = new Game("a", "abc", 5);
-        Game b = new Game("b", "abc", 7);
-        Game c = new Game("c", "abc", 3);
-        Game d = new Game("d", "abc", 8);
+        Score a = new Score(1, 1, 2, 145.6, DateTime.Parse("07-12-2022"));
+        Score b = new Score(2, 1, 2, 148.2, DateTime.Parse("07-12-2022"));
+        Score c = new Score(3, 1, 2, 159.8, DateTime.Parse("07-12-2022"));
+        Score d = new Score(4, 3, 2, 137.0, DateTime.Parse("07-12-2022"));
+        Score e = new Score(1, 3, 1, 145.6, DateTime.Parse("07-12-2022"));
+        Score f = new Score(2, 1, 2, 178.2, DateTime.Parse("07-12-2022"));
+        
+        Sistema.ScoreInserir(a);
+        Sistema.ScoreInserir(b);
+        Sistema.ScoreInserir(c);
+        Sistema.ScoreInserir(d);
+        Sistema.ScoreInserir(e);
+        Sistema.ScoreInserir(f);
 
-        Sistema.GameInserir(a);
-        Sistema.GameInserir(b);
-        Sistema.GameInserir(c);
-        Sistema.GameInserir(d);
+        Sistema.ScoreExcluir(2);
 
-        Sistema.GameExcluir(2);
-
-        foreach (Game g in Sistema.GameListar())
-            Console.WriteLine(g);
+        foreach (Score s in Sistema.ScoreListar(1, 2))
+            Console.WriteLine(s);
     }
 }
