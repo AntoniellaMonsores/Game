55e7b00387e9e79b5aa5b6fec6ab4b0c98f6138e
Antoniella Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-03-07 13:25:17 -0300

    novo teste

--- main.cs
+++ main.cs
@@ -4,25 +4,78 @@ class Program {
 
     public static void Main(string[] args)
     {
-        Score a = new Score(1, 1, 2, 145.6, DateTime.Parse("07-12-2022"));
-        Score b = new Score(2, 1, 2, 148.2, DateTime.Parse("07-12-2022"));
-        Score c = new Score(3, 1, 2, 159.8, DateTime.Parse("07-12-2022"));
-        Score d = new Score(4, 3, 2, 137.0, DateTime.Parse("07-12-2022"));
-        Score e = new Score(1, 3, 1, 145.6, DateTime.Parse("07-12-2022"));
-        Score f = new Score(2, 1, 2, 178.2, DateTime.Parse("07-12-2022"));
+        int func;
         
-        Sistema.ScoreInserir(a);
-        Sistema.ScoreInserir(b);
-        Sistema.ScoreInserir(c);
-        Sistema.ScoreInserir(d);
-        Sistema.ScoreInserir(e);
-        Sistema.ScoreInserir(f);
-
-        Sistema.ScoreAtualizar(obj);
+        Console.WriteLine("Bem-vindo(a)!");
+        Console.WriteLine();
         
-        Sistema.ScoreExcluir(2);
+        Console.WriteLine("O que deseja?");
+        Console.WriteLine("00 - Cadastrar jogo");
+        Console.WriteLine("01 - Cadastrar jogador");
+        Console.WriteLine("02 - Cadastrar pontuação");
+        Console.WriteLine("03 - Visualizar jogos cadastrados");
+        Console.WriteLine("04 - Visualizar jogadores cadastrados");
+        Console.WriteLine("05 - Visualizar pontuação");
+        Console.WriteLine();
 
-        foreach (Score s in Sistema.ScoreListar(1, 2))
-            Console.WriteLine(s);
+        func = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        switch (func) 
+        {
+            case 0 : CadastrarGame(); 
+                break;
+            case 1 : CadastrarUser(); 
+                break;
+            case 2 : CadastrarScore();
+                break;
+        }
+
     }
+    
+    public static void CadastrarGame() 
+    {
+        Console.WriteLine("Informações referentes ao jogo");
+        Console.WriteLine();
+        
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Gênero: ");
+        string genero = Console.ReadLine();
+
+        Console.Write("Quantidade de níveis: ");
+        int niveis = int.Parse(Console.ReadLine());
+        
+        Game game = new Game(nome, genero, niveis);
+        Sistema.GameInserir(game);
+    }
+
+    public static void CadastrarUser() 
+    {        
+        Console.WriteLine("Informações referentes ao jogador");
+        Console.WriteLine();
+        
+        Console.Write("Idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.Write("Apelido: ");
+        string id = Console.ReadLine();
+        
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+        
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+
+        Player user = new Player(idade, id, nome, email);
+        Sistema.PlayerInserir(user);
+    }
+
+    public static void CadastrarScore() 
+    {
+        
+        
+    }
+    
 }
