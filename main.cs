cc9854e3fb1dc406a1f91d4702769d1d0d9bf5fe
Antoniella de Araújo Monsores <antoniella.monsores@gmail.com>
2022-03-08 11:00:43 -0300

    teste

--- main.cs
+++ main.cs
@@ -24,17 +24,23 @@ class Program {
                     break;
                 case 3 : CadastrarScore();
                     break;
-                case 4 : ListarGames();
+                case 4 : AtualizarGames();
                     break;
-                case 5 : ListarUsers();
+                case 5 : AtualizarUsers();
                     break;
-                case 6 : ListarScore();
+                case 6 : AtualizarScore();
                     break;
                 case 7 : ExcluirGame();
                     break;
                 case 8 : ExcluirUser();
                     break;
                 case 9 : ExcluirScore();
+                    break;
+                case 10 : ListarGames();
+                    break;
+                case 11 : ListarUsers();
+                    break;
+                case 12 : ListarScore();
                     break;
             }
         }
@@ -52,12 +58,15 @@ class Program {
         Console.WriteLine("01 - Cadastrar jogo");
         Console.WriteLine("02 - Cadastrar jogador");
         Console.WriteLine("03 - Cadastrar pontuação");
-        Console.WriteLine("04 - Visualizar jogos cadastrados");
-        Console.WriteLine("05 - Visualizar jogadores cadastrados");
-        Console.WriteLine("06 - Visualizar pontuação");
+        Console.WriteLine("04 - Atualizar jogo");
+        Console.WriteLine("05 - Atualizar jogador");
+        Console.WriteLine("06 - Atualizar pontuação");
         Console.WriteLine("07 - Excluir jogo");
         Console.WriteLine("08 - Excluir jogador");
         Console.WriteLine("09 - Excluir pontuação");
+        Console.WriteLine("10 - Visualizar jogos cadastrados");
+        Console.WriteLine("11 - Visualizar jogadores cadastrados");
+        Console.WriteLine("12 - Visualizar pontuação");
         Console.WriteLine("00 - Sair do sistema");
         Console.WriteLine();
 
@@ -171,7 +180,7 @@ class Program {
         int nivel = int.Parse(Console.ReadLine());
 
         Console.Write("Pontuação: ");
-        double pontos = int.Parse(Console.ReadLine());
+        double pontos = double.Parse(Console.ReadLine());
 
         Console.Write("Data: ");
         DateTime data = DateTime.Parse(Console.ReadLine());
@@ -180,55 +189,19 @@ class Program {
         Sistema.ScoreInserir(score);
     }
 
-    public static void ListarGames()
+    public static void AtualizarGames()
     {
-        // Listar games
-        // Listagem de todos os jogos atualmente cadastrados no sistema
-
-        Game[] games = Sistema.GameListar();
-        
-        Console.WriteLine();
-        
-        for (int i = 0; i < games.Length; i++)
-            Console.WriteLine($"{games[i]}");
+ 
     }
-    
-    public static void ListarUsers()
-    {
-        // Listar users
-        // Listagem de todos os jogadores atualmente cadastrados no sistema
 
-        List<Player> users = Sistema.PlayerListar();
-        
-        Console.WriteLine();
-        
-        for (int i = 0; i < users.Count; i++)
-            Console.WriteLine($"{users[i]}");
+    public static void AtualizarUsers()
+    {
+ 
     }
-    
-    public static void ListarScore()
+
+    public static void AtualizarScore()
     {
-        // Listar score
-        // Listagem de todos os scores associados a um jogo e jogador específicos
-        // Possível visualização dos jogos e jogadores cadastrados no sistema
-
-        MenuSecundario();
-        Console.WriteLine();
-        Console.WriteLine("A pontuação será dada de acordo com o jogo e jogador informado");
-        Console.WriteLine();
-        
-        Console.Write("Numeração do jogo: ");
-        int idGame = int.Parse(Console.ReadLine());
-
-        Console.Write("Numeração do usuário: ");
-        int idUser = int.Parse(Console.ReadLine());
-        
-        List<Score> score = Sistema.ScoreListar(idGame, idUser);
-        
-        Console.WriteLine();
-        
-        for (int i = 0; i < score.Count; i++)
-            Console.WriteLine($"{score[i]}");
+ 
     }
 
     public static void ExcluirGame()
@@ -303,6 +276,75 @@ class Program {
 
     public static void ExcluirScore()
     {
- 
+        // Excluir user
+        // Faz uso da classe ListarScore()
+        // Resulta na exclusão do score correspondente ao id informado
+
+        int id;
+        int[] val = ListarScore();
+
+        Console.WriteLine();
+        Console.Write("Numeração do score: ");
+        
+        id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Sistema.ScoreExcluir(Sistema.ScoreListar(id, val[0], val[1]));
+    }
+
+    public static void ListarGames()
+    {
+        // Listar games
+        // Listagem de todos os jogos atualmente cadastrados no sistema
+
+        Game[] games = Sistema.GameListar();
+        
+        Console.WriteLine();
+        
+        for (int i = 0; i < games.Length; i++)
+            Console.WriteLine($"{games[i]}");
+    }
+    
+    public static void ListarUsers()
+    {
+        // Listar users
+        // Listagem de todos os jogadores atualmente cadastrados no sistema
+
+        List<Player> users = Sistema.PlayerListar();
+        
+        Console.WriteLine();
+        
+        for (int i = 0; i < users.Count; i++)
+            Console.WriteLine($"{users[i]}");
+    }
+    
+    public static int[] ListarScore()
+    {
+        // Listar score
+        // Listagem de todos os scores associados a um jogo e jogador específicos
+        // Possível visualização dos jogos e jogadores cadastrados no sistema
+
+        int idGame;
+        int idUser;
+
+        MenuSecundario();
+    
+        Console.Write("Numeração do jogo: ");
+        idGame = int.Parse(Console.ReadLine());
+
+        Console.Write("Numeração do usuário: ");
+        idUser = int.Parse(Console.ReadLine());
+        
+        List<Score> score = Sistema.ScoreListar(idGame, idUser);
+        
+        Console.WriteLine();
+        
+        for (int i = 0; i < score.Count; i++)
+            Console.WriteLine($"{i:0000} - {score[i]}");
+
+        int[] val = {idGame, idUser};
+
+        return val;
     }
 }
