212be23b0637b4bffe427c18c59e8e8beec1602a
leticiamaiaa <79123176+leticiamaiaa@users.noreply.github.com>
2022-03-23 12:13:12 +0000

    update

--- arquivo.cs
+++ arquivo.cs
@@ -8,4 +8,56 @@ class Arquivo<T> {
   XmlSerializer xml = new XmlSerializer(typeof(T));
   StreamReader sw = new StreamReader(arquivo, Encoding.Default);
   T obj = (T) xml.Deserialize(sw);
+  sw.Close();
 }
+
+    /* Passar para outro arquivo
+
+    try
+    { 
+        // Passa a tragetoria e o nome do arquivo para o construtor StreamWriter
+        StreamReader sr = new StreamReader("teste.txt");
+        string data  = sr.ReadLine();
+        
+        // Ler até o fim do arquivo
+        while (data != null)
+        {
+          Console.WriteLine("data");
+            
+          //ler a prox line
+          data = sr.ReadLine();
+        }
+        
+        //fechando o arquivo
+        sr.Close();
+        Console.ReadLine();
+    }
+        
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);    
+    }
+        
+    finally
+    {
+        Console.WriteLine("executando o ultimo");
+    }
+      
+    try
+    {
+        StreamWriter sw = new StreamWriter("teste.txt");
+        sw.WriteLine("testando");
+        sw.WriteLine("testando2");
+        sw.Close();
+    }
+        
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);    
+    }
+        
+    finally
+    {
+        Console.WriteLine("executando o ultimo");
+    }
+    */
